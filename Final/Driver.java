422b14a29ed2c6a4b546e8c45fd2fac0e5bf5270
Alexis Dionne <alexis.dionne98@gmail.com>
2019-05-06 11:28:09 -0400

    It was working until I added avoid. I think it is just a tuning/math problem.

--- Final/Driver.java
+++ Final/Driver.java
@@ -3,7 +3,9 @@ import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.MovePilot;
+import lejos.utility.Delay;
 import lejos.hardware.Keys;
+import lejos.hardware.Sound;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.Port;
@@ -12,142 +14,171 @@ import lejos.hardware.sensor.NXTUltrasonicSensor;
 
 public class Driver{
 	
-	double[] pose = {0.0, 0.0, 1.5708}; // [x, y, theta(radians)]
-	double[] goal = {100.0, 100.0};
-	double circumference = 3.5*3.14;//maybe 3.5
-	
-	float[] obstacle_data = new float[3];
+	//					  y, x
+	static int[] goal = {60, 30};
+	static float[] obstacle_data = new float[5];
 	int dataCounter = 0;
 	
-	//SampleProvider distLeft;
-	//SampleProvider distRight;
-	//float[] sampleDistLeft;
-	//float[] sampleDistRight;
-	SampleProvider distSensor;
-	float[] sampleDist;
-	
-	//NXTUltrasonicSensor uSensorLeft;
-	//NXTUltrasonicSensor uSensorRight;
-	NXTUltrasonicSensor uSensor;
-	EV3LargeRegulatedMotor motorA;
-	NXTRegulatedMotor motorB;
-	EV3LargeRegulatedMotor motorD;
+	// initialize ports
+	static Port port4 = LocalEV3.get().getPort("S4");
+	static Port portA = LocalEV3.get().getPort("A");  // right
+	static Port portB = LocalEV3.get().getPort("B");
+	static Port portD = LocalEV3.get().getPort("D");  // left
+	static NXTUltrasonicSensor uSensor = new NXTUltrasonicSensor(port4);
+	static EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(portA);
+	static EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(portD);
+	static NXTRegulatedMotor motorB = new NXTRegulatedMotor(portB);
+
+	static SampleProvider distSensor = uSensor.getDistanceMode();
+	static float[] sampleDist = new float[distSensor.sampleSize()];
 	static Keys buttons = LocalEV3.get().getKeys();
 	
-	Chassis chassis;
-	MovePilot pilot;
-	
-	public Driver() {
-		
-		// initialize ports
-		Port portS1 = LocalEV3.get().getPort("S1");
-		Port portS4 = LocalEV3.get().getPort("S4");
-		Port portA = LocalEV3.get().getPort("A");  // right
-		Port portB = LocalEV3.get().getPort("B");
-		Port portD = LocalEV3.get().getPort("D");  // left
+	static double theta = 0.0;
+	static double deltaS = 0.0;
+	static double x = 0.0;
+	static double y = 0.0;
+	static double deltaTheta = 0.0;
 
-		// assign sensors to ports
-		//uSensorLeft = new NXTUltrasonicSensor(portS4);
-		//uSensorRight = new NXTUltrasonicSensor(portS1);
-		uSensor = new NXTUltrasonicSensor(portS4);
-		
-		// initialize motors and pilot
-		motorA = new EV3LargeRegulatedMotor(portA);
-		motorD = new EV3LargeRegulatedMotor(portD);
-		motorB = new NXTRegulatedMotor(portB);
-		//motorB.rotateTo(90);
-		motorA.setSpeed(200);
-		motorD.setSpeed(200);
-		motorA.forward();
-		motorD.forward();
-		Wheel wheelA = WheeledChassis.modelWheel(motorA, 43.2).offset(-72);
-		Wheel wheelD = WheeledChassis.modelWheel(motorD, 43.2).offset(72);
-		chassis = new WheeledChassis(new Wheel[]{wheelA, wheelD}, WheeledChassis.TYPE_DIFFERENTIAL);
-		pilot = new MovePilot(chassis);
-		
-		//distLeft = uSensorLeft.getDistanceMode();
-		//sampleDistLeft = new float[distLeft.sampleSize()];
-		//distRight = uSensorRight.getDistanceMode();
-		//sampleDistRight = new float[distRight.sampleSize()];
-		distSensor = uSensor.getDistanceMode();
-		sampleDist = new float[distSensor.sampleSize()];
-		
-		
-		localize();
-	}
+	static int counter = 0;
+	static int position = 0;
+	
+	static int currentRightTacho, currentLeftTacho;
+	static int prevRightTacho = 0, prevLeftTacho = 0;
+	static int rightSpeed = 200, leftSpeed = 200;
+	
+	static double radius = 1.9;
+	static double width = 17.5;
+	static double wheelDistance = (3.14 * 2 * radius) / 360;
+
 
 	public static void main(String[] args) {
 
 		System.out.print("Hit a button!!");
 		buttons.waitForAnyPress();
+		int count = 0;
+		theta = 0;
+		deltaTheta = 0;
+		deltaS = 0;
 		LCD.clearDisplay();
+		
+		while (count < 1) {
+			LCD.drawString("goal x:"+goal[1], 0, 6);
+			LCD.drawString("goal y:"+goal[0], 0, 7);
+			
+			if(        Math.abs(x) >= (Math.abs(goal[0] - 2)) 
+					&& Math.abs(x) <= (Math.abs(goal[0] + 2)) 
+					&& Math.abs(y) >= (Math.abs(goal[1] - 2)) 
+					&& Math.abs(y) <= (Math.abs(goal[1] + 2))) {
+				motorRight.stop();
+				motorLeft.stop();
+				Sound.beepSequenceUp();
+				count++;
+				Delay.msDelay(3000);
+			}
+			else {
+				currentLeftTacho = motorLeft.getTachoCount();
+				currentRightTacho = motorRight.getTachoCount();
 
-		new Driver();
+				localize(currentRightTacho, currentLeftTacho);
+			}
+		}
+
 	}
 
-	public void localize() {
-		boolean end = false;
-		int counter = 0;
-		int position = 0;
-		double Sr = 0, Sl = 0, lastSrTach = 0, lastSlTach = 0;
+	public static void localize(int rightTacho, int leftTacho) {
+		double Sr = 0, Sl = 0;
 		double goalTheta = 0;
-		while(end == false) {
-			// loop through algorithm until a button is pressed
+		double kp = 100;
+		double ke = 100;
+		double avoidError = 0;
 			
-			if(counter % 20 == 0) {
-				// gather fresh data
-				//distLeft.fetchSample(sampleDistLeft, 0);
-				//distRight.fetchSample(sampleDistRight, 0);
-				
-				//obstacle_data[0] = sampleDistLeft[0];
-				//obstacle_data[1] = sampleDistRight[0];
-				
-				distSensor.fetchSample(sampleDist, 0);
-				obstacle_data[position] = sampleDist[0];
-				//System.out.println("pos "+position+" "+sampleDist[0]);
-				
-				if(position == 2) {
-					motorB.rotate(-180);
-					position = 0;
-				}
-				else {
-					motorB.rotate(90);
-					position++;
-				}
-
-				
+		if(counter % 10  == 0) {
+			// gather fresh data
+			distSensor.fetchSample(sampleDist, 0);
+			obstacle_data[position] = sampleDist[0];
+			
+			if(position == 4) {
+				motorB.rotate(-180);
+				position = 0;
 			}
-			// get goal direction
-			// find distances traveled by wheels since last math
-			Sr = circumference*((motorA.getTachoCount()-lastSrTach)/360);//maybe get tacho count
-			Sl = circumference*((motorD.getTachoCount()-lastSlTach)/360);
-			lastSrTach = motorA.getTachoCount();
-			lastSlTach = motorD.getTachoCount();
-			System.out.println(Sr+" "+Sl);
-			
-			double deltaS = (Sr+Sl)/2;
-			double deltaTheta = (Sr-Sl)/70;
-			
-			double deltaX = deltaS*Math.cos(pose[2]+(deltaTheta/2));
-			double deltaY = deltaS*Math.sin(pose[2]+(deltaTheta/2));
-			goalTheta = Math.atan2(goal[1]-deltaY, goal[0]-deltaX);
-			
-			//if(Math.toDegrees(goalTheta) {
-			int speedA = (int) Math.sqrt(Math.pow((goal[1]-deltaX), 2) + Math.pow((deltaY-goal[1]), 2));
-			int speedD = (int) Math.sqrt(Math.pow((goal[1]-deltaX), 2) + Math.pow((deltaY-goal[1]), 2));
-
-			// get objects in the path to push away from
-			
-			//System.out.println("x:"+deltaX+" y:"+deltaY+" theta"+goalTheta);
-			//System.out.println("speed right:"+speedA+" left: "+speedD);
-	
-			motorA.setSpeed(speedA);
-			motorD.setSpeed(speedD);
-			
-			if(buttons.readButtons() != 0) {
-				end = true;
+			else {
+				motorB.rotate(45);
+				position++;
 			}
-			counter++;
 		}
+		// get goal direction
+		// find distances traveled by wheels since last math
+		Sr = (rightTacho - prevRightTacho)*wheelDistance;//maybe get tacho count
+		Sl = (leftTacho - prevLeftTacho)*wheelDistance;
+		prevRightTacho = rightTacho;
+		prevLeftTacho = leftTacho;
+		
+		double deltaS = (Sr+Sl)/2;
+		double deltaTheta = (Sr-Sl)/width;
+		
+		double deltaX = deltaS*Math.cos(theta+(deltaTheta/2));
+		double deltaY = deltaS*Math.sin(theta+(deltaTheta/2));
+		
+		theta += deltaTheta;
+		x += deltaX;
+		y += deltaY;
+		
+		LCD.drawString("X: "+y, 0, 3);
+		LCD.drawString("Y: "+x, 0, 4);
+		
+		goalTheta = Math.atan2(goal[1] - y, goal[0] - x);
+
+		double error = goalTheta - theta;
+		if((x <= goal[0] + 20 && x >= goal[0] - 20) && (y <= goal[1] + 20 && y >= goal[1] - 20)) {
+			avoidError = 0;
+			LCD.drawString("          ", 0, 5);
+		}
+		else {
+			avoidError = avoid();
+			LCD.drawString("AVOID: "+avoidError, 0, 5);
+		}
+		
+		int currRight = (int) (rightSpeed + (kp*error+ke*avoidError));
+		int currLeft = (int) (leftSpeed - (kp*error+ke*avoidError));
+		
+		motorRight.setSpeed(currRight);
+		motorLeft.setSpeed(currLeft);
+		
+		if(currRight > 0) {
+			motorRight.forward();
+		}
+		else {
+			motorRight.setSpeed((int) Math.abs(currRight));
+			motorRight.backward();
+		}
+		if(currLeft > 0) {
+			motorLeft.forward();
+		}
+		else {
+			motorLeft.setSpeed((int) Math.abs(currLeft));
+			motorLeft.backward();
+		}
+		counter++;
+	}
+	
+	public static double avoid() {
+		double avoidError = 0;
+		if(obstacle_data[4] < .85) {
+			avoidError += (obstacle_data[0] - .85);
+		}
+		if(obstacle_data[3] < .50) {
+			avoidError += (obstacle_data[1] - .50);
+		}
+		if(obstacle_data[2] < .20) {
+			avoidError += (1 - obstacle_data[2]);
+		}
+		if(obstacle_data[1] < .50) {
+			avoidError += (.50 - obstacle_data[3]);
+		}
+		if(obstacle_data[0] < .85) {
+			avoidError += (.85 - obstacle_data[4]);
+		}
+		
+		return avoidError;
 	}
 }
