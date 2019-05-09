5f8b6014971f421bc7eea83b4b5fcdd9786fe12b
Alexis Dionne <alexis.dionne98@gmail.com>
2019-05-09 10:01:39 -0400

    Cleaned up the code for the driver. Comments were added and untouched code was removed.

--- Final/Driver.java
+++ Final/Driver.java
@@ -1,8 +1,4 @@
 import lejos.robotics.SampleProvider;
-import lejos.robotics.chassis.Chassis;
-import lejos.robotics.chassis.Wheel;
-import lejos.robotics.chassis.WheeledChassis;
-import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
 import lejos.hardware.Keys;
 import lejos.hardware.Sound;
@@ -12,12 +8,14 @@ import lejos.hardware.port.Port;
 import lejos.hardware.motor.*;
 import lejos.hardware.sensor.NXTUltrasonicSensor;
 
+// CMPT 412 Robotics
+// Final Project - Potential Field Path Finding and Localization
+// Created by: Alexis Dionne, Nic Mays, and Jack Hueber
+
 public class Driver{
 	
-	//					  y, x
-	static int[] goal = {40, 30};
+	static int[] goal = {600, -250}; // it actually goes {y, x} when inserting coordinates
 	static float[] obstacle_data = new float[3];
-	int dataCounter = 0;
 	
 	// initialize ports
 	static Port port4 = LocalEV3.get().getPort("S4");
@@ -29,24 +27,27 @@ public class Driver{
 	static EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(portD);
 	static NXTRegulatedMotor motorB = new NXTRegulatedMotor(portB);
 
+	//initialize sensor
 	static SampleProvider distSensor = uSensor.getDistanceMode();
 	static float[] sampleDist = new float[distSensor.sampleSize()];
 	static Keys buttons = LocalEV3.get().getKeys();
 	
+	// Math variables are global
 	static double theta = 0.0;
 	static double deltaS = 0.0;
 	static double x = 0.0;
 	static double y = 0.0;
 	static double deltaTheta = 0.0;
 
+	// variables to keep track of the sensor position
 	static int counter = 0;
 	static int position = 0;
 	static boolean reverse = false;
 	
+	// wheel variables
 	static int currentRightTacho, currentLeftTacho;
 	static int prevRightTacho = 0, prevLeftTacho = 0;
 	static int rightSpeed = 200, leftSpeed = 200;
-	
 	static double radius = 1.9;
 	static double width = 17.5;
 	static double wheelDistance = (3.14 * 2 * radius) / 360;
@@ -63,7 +64,7 @@ public class Driver{
 		LCD.clearDisplay();
 		
 		for(int x = 0; x < 3; x++) {
-			// get data consistently for freshest readings
+			// grab data around robot before moving out to watch for close obstacles
 			distSensor.fetchSample(sampleDist, 0);
 			obstacle_data[x] = sampleDist[0];
 			if(x<2) {
@@ -81,13 +82,27 @@ public class Driver{
 					&& Math.abs(x) <= (Math.abs(goal[0] + 4)) 
 					&& Math.abs(y) >= (Math.abs(goal[1] - 4)) 
 					&& Math.abs(y) <= (Math.abs(goal[1] + 4))) {
+				// within error range of goal
 				motorRight.stop();
 				motorLeft.stop();
 				Sound.beepSequenceUp();
 				count++;
 				Delay.msDelay(3000);
 			}
+			else if(Math.abs(x) > Math.abs(goal[0]) && Math.abs(y) > Math.abs(goal[1])) {
+				// oops we passed it so stop instead of flailing
+				motorRight.stop();
+				motorLeft.stop();
+				Sound.beepSequence();
+				count++;
+				Delay.msDelay(3000);
+			}
+			else if (buttons.readButtons() == Keys.ID_ENTER) {
+				// button was pressed to end this run
+				count++;
+			}
 			else {
+				// get newest movement measurements and localize
 				currentLeftTacho = motorLeft.getTachoCount();
 				currentRightTacho = motorRight.getTachoCount();
 
@@ -98,17 +113,19 @@ public class Driver{
 	}
 
 	public static void localize(int rightTacho, int leftTacho) {
+		// find the shortest path to a goal location without hitting any objects
+		
 		double Sr = 0, Sl = 0;
 		double goalTheta = 0;
-		double kp = 100;
-		double ke = 200;
+		double kp = 100; // theta constant
+		double ke = 250; // avoid error constant
 		double avoidError = 0;
 		
-		// get data consistently for freshest readings
+		// get data every time for freshest readings
 		distSensor.fetchSample(sampleDist, 0);
 		obstacle_data[position] = sampleDist[0];
-		System.out.println("position: "+position);
-		System.out.println("sample: "+sampleDist[0]);
+
+		// update ultrasonic sensor head direction
 		if(position == 2) {
 			reverse = true;
 			motorB.rotate(-45);
@@ -131,8 +148,8 @@ public class Driver{
 		}
 		
 		// get goal direction
-		// find distances traveled by wheels since last math
-		Sr = (rightTacho - prevRightTacho)*wheelDistance;//maybe get tacho count
+		// find distances traveled by wheels since last calculation
+		Sr = (rightTacho - prevRightTacho)*wheelDistance;
 		Sl = (leftTacho - prevLeftTacho)*wheelDistance;
 		prevRightTacho = rightTacho;
 		prevLeftTacho = leftTacho;
@@ -149,68 +166,53 @@ public class Driver{
 		
 		LCD.drawString("X: "+y, 0, 3);
 		LCD.drawString("Y: "+x, 0, 4);
-		System.out.println("X: "+y);
-		System.out.println("Y: "+x);
 		
+		// goal theta is the direction to want to turn to re-orient with the end location
 		goalTheta = Math.atan2(goal[1] - y, goal[0] - x);
-
 		double error = goalTheta - theta;
+		
 		if((x <= goal[0] + 20 && x >= goal[0] - 20) && (y <= goal[1] + 20 && y >= goal[1] - 20)) {
+			// if we're close to the end goal, go for it and don't check for other obstacles
 			avoidError = 0;
 			LCD.drawString("          ", 0, 5);
 		}
 		else {
-			//avoidError = 0;
 			avoidError = avoid();
 			LCD.drawString("AVOID: "+avoidError, 0, 5);
 		}
 		
+		// calculate speeds to adjust to when combining values of goal error and avoid error
+		// left values are negative
 		int currRight = (int) (rightSpeed + (kp*error+ke*avoidError));
 		int currLeft = (int) (leftSpeed - (kp*error+ke*avoidError));
-		System.out.println("speedRight:"+currRight+"\nspeedLEft:"+currLeft);
+
 		motorRight.setSpeed(currRight);
 		motorLeft.setSpeed(currLeft);
 		
 		if(currRight > 0) {
 			motorRight.forward();
 		}
-		else {
-			motorRight.setSpeed((int) Math.abs(currRight));
-			motorRight.backward();
-		}
 		if(currLeft > 0) {
 			motorLeft.forward();
-		}
-		else {
-			motorLeft.setSpeed((int) Math.abs(currLeft));
-			motorLeft.backward();
 		}
 		counter++;
 	}
 	
 	public static double avoid() {
+		// avoid is calculated by taking the data (if it matters) and adding it all 
+		// to make a solid avoid error value. Left obstacles result in negative values
 		double avoidError = 0;
-		/*if(obstacle_data[4] < .45 && obstacle_data[4] != 0) {
-			avoidError += (obstacle_data[4] - .45);
-			System.out.println("4: "+avoidError);
-		}*/
+		
 		if(obstacle_data[2] < .60 && obstacle_data[2] != 0) {
 			avoidError += (obstacle_data[2] - .60);
-			System.out.println("2: "+avoidError);
 		}
 		if(obstacle_data[1] < .80 && obstacle_data[1] != 0) {
+			// robot favors avoiding left
 			avoidError += (1 - obstacle_data[1]);
-			System.out.println("1: "+avoidError);
 		}
 		if(obstacle_data[0] < .60 && obstacle_data[0] != 0) {
 			avoidError += (.60 - obstacle_data[0]);
-			System.out.println("0: "+avoidError);
 		}
-		/*if(obstacle_data[0] < .45 && obstacle_data[0] != 0) {
-			avoidError += (.45 - obstacle_data[0]);
-			System.out.println("0: "+avoidError);
-		}*/
-		System.out.println("Error: "+avoidError);
 		return avoidError;
 	}
 }
